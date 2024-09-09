49f421f8a67bb3d71806b52186e0f38a402378f2
MinhLe <letuanminh311@gmail.com>
2024-09-09 10:26:18 +0700

    Add SelectionSort and InsertionSort

--- J1.S.P0001/src/Utils.java
+++ J1.S.P0001/src/Utils.java
@@ -30,4 +30,32 @@ public class Utils {
             }
         }
     }
+
+    public static void SelectionSort(int[] arr) {
+        int n = arr.length;
+        for(int i=0;i<n-1;i++){
+            int min = i;
+            for(int j=i+1;j<n;j++){
+                if(arr[j]<arr[min]){
+                    min = j;
+                }
+            }
+            int temp = arr[i];
+            arr[i] = arr[min];
+            arr[min] = temp;
+        }
+    }
+    public static void InsertionSort(int[] array) {
+        int n = array.length;
+        for (int i = 1; i < n; i++) {
+            int key = array[i];
+            int j = i - 1;
+            while (j >= 0 && array[j] > key) {
+                array[j + 1] = array[j];
+                j = j - 1;
+            }
+            array[j + 1] = key;
+        }
+    }
+
 }
